f022a4ef3bef0268eb5b71ebef0582c76b75fb45
fushuwei <331044761@qq.com>
2022-09-24 17:34:57 +0800

    增加 editorconfig 和 .eslintignore 文件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,12 +19,7 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-        "@typescript-eslint/ban-types": ["error", {
-            "extendDefaults": true,
-            "types": {
-                "{}": false
-            }
-        }],
-        "@typescript-eslint/no-explicit-any": ["off"]
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
     }
 }
